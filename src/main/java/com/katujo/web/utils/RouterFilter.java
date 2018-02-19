2b08bb0c10d21e8780b55fe05095ce55f50317a1
jhertz <jhertz@katujo08>
2018-02-19 13:04:00 +0100

    Clean up.

--- src/main/java/com/katujo/web/utils/RouterFilter.java
+++ src/main/java/com/katujo/web/utils/RouterFilter.java
@@ -18,7 +18,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
