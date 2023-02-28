8547b1842ab8580292a37d90f102b9091e870d9c
jhertz <jhertz@DESKTOP-K48Q5N7>
2023-02-28 16:20:58 +0100

    Added mapping for oracle.sql.TIMESTAMP.

--- src/main/java/com/katujo/web/utils/JsonUtils.java
+++ src/main/java/com/katujo/web/utils/JsonUtils.java
@@ -572,8 +572,9 @@ public class JsonUtils
 				else if(Float.class.getName().equals(type)) types[column] = DatabaseTypes.DOUBLE;
 				else if(BigDecimal.class.getName().equals(type) || type.contains("BINARY_DOUBLE")) types[column] = DatabaseTypes.DOUBLE;
 				else if(Integer.class.getName().equals(type)) types[column] = DatabaseTypes.INTEGER;
-				else if(Long.class.getName().equals(type) || BigInteger.class.getName().equals(type)) types[column] = DatabaseTypes.LONG;				
-				else if(java.sql.Timestamp.class.getName().equals(type)) types[column] = DatabaseTypes.TIMESTAMP;
+				else if(Long.class.getName().equals(type) || BigInteger.class.getName().equals(type)) types[column] = DatabaseTypes.LONG;
+				else if("oracle.sql.TIMESTAMP".equals(type)) types[column] = DatabaseTypes.TIMESTAMP;
+				else if(java.sql.Timestamp.class.getName().equals(type)) types[column] = DatabaseTypes.TIMESTAMP;							
 				else if(java.sql.Time.class.getName().equals(type)) types[column] = DatabaseTypes.TIMESTAMP; //TODO: Implement the time database type
 				else if(java.sql.Date.class.getName().equals(type)) types[column] = DatabaseTypes.DATE;
 				else if(type.toUpperCase().endsWith(".CLOB") || type.toUpperCase().endsWith(".ORACLECLOB") || "BINARY".equals(meta.getColumnTypeName(column+1))) types[column] = DatabaseTypes.STRING;
